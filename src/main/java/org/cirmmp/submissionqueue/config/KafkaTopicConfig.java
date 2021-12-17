99e2d9b908dd5f2094d1ccd3fac306c974c277a5
Andrea <andreagia71@gmail.com>
2021-12-17 15:41:20 +0100

    kafka configration topic

--- src/main/java/org/cirmmp/submissionqueue/config/KafkaTopicConfig.java
+++ src/main/java/org/cirmmp/submissionqueue/config/KafkaTopicConfig.java
@@ -1,10 +1,15 @@
 package org.cirmmp.submissionqueue.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 class KafkaTopicConfig {
@@ -14,6 +19,16 @@ class KafkaTopicConfig {
 
     @Value("${io.reflectoring.kafka.topic-2}")
     private String topic2;
+
+    @Value("${io.reflectoring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return new KafkaAdmin(configs);
+    }
 
     @Bean
     NewTopic topic1() {
